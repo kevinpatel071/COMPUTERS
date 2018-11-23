d950fb24e7aefc39bb6330bd65282c2b300c8024
kevinpatel071 <kevinpatel038@gmail.com>
2018-11-23 09:37:58 +0530

    login2 commit

---
+++ Login.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<center>
+<h1>Login Page</h1>
+<form action='welcom.php'>
+User Name :<input type='text' name='txtuname'><br/>
+Password :<input type='password' name='txtpass'><br/>
+<input type='submit' value='Login'>
+</form>
+</center>
+</body>
+</html>
